e3664a892881f24e233ae86e1508be509fcbbb69
narendra237 <91244568+narendra237@users.noreply.github.com>
2022-10-08 20:04:56 +0530

    Update takeInputUsingQueue.cpp

--- TREES/takeInputUsingQueue.cpp
+++ TREES/takeInputUsingQueue.cpp
@@ -70,9 +70,16 @@ void PrintAtLevelK(TreeNode<int>* root, int k){
 
 
 int getLeafNodeCount(TreeNode<int>* root) {
-    if(root == NULL)
+     if(root == NULL)
         return 0;
-    if(root->data)
+    if(root->children.size() == 0)
+        return 1;
+    int count=0;
+    for(int i = 0;i<root->children.size();i++){
+        count=count + getLeafNodeCount(root->children[i]);
+    }
+   // count++;
+    return count;
 }
 
 int TotalNodes(TreeNode <int> * root){
