ab892348b895d05afb47d258117842054b123a12
narendra237 <91244568+narendra237@users.noreply.github.com>
2022-10-16 03:26:44 +0530

    Create InitialisationList.cpp

---
+++ OOPs Concept/InitialisationList.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include "InitialisattionListClass.h"
+
+int main(){
+    Student s1(101,20);
+    s1.age = 20;
+    //s1.rollNumber = 101;
+    return 0;
+}
