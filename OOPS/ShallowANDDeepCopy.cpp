d766439e67be9b35f2b4398eb2c6b0911eb28602
narendra237 <91244568+narendra237@users.noreply.github.com>
2022-10-16 01:39:23 +0530

    Update ShallowANDDeepCopy.cpp

--- OOPS/ShallowANDDeepCopy.cpp
+++ OOPS/ShallowANDDeepCopy.cpp
@@ -1,26 +1,6 @@
 #include<bits/stdc++.h>
+#include"StudentClass.h"
 using namespace std;
-
-class Student{
-    int age;
-    char* name;
-
-    public:
-
-    Student(int age , char * name){
-        this->age = age;
-        // this->name = name;  known as shallow Copy
-        this->name = new char[strlen(name)+1];
-        strcpy(this->name, name);
-
-        // above two line shows us about deep copy , it copies entire name array with a new address and the orignal
-        // array remain untouched and can accessible through every object;
-    }
-
-    void display(){
-        cout<<name<<" "<<age<<endl;
-    }
-};
 
 int main(){
     char name[] = "abcd";
@@ -34,6 +14,12 @@ int main(){
     s1.display();// in this piece of code, the address of code is same for every displayy output
     // let se the address of name is {500}. So the every time we change the character in name, it
     // changes for every upcoming data and reflects in previous data also  like for above line .
+
+    // this->name = new char[strlen(name)+1];
+    // strcpy(this->name, name);
+
+        // above two line shows us about deep copy , it copies entire name array with a new address and the orignal
+        // array remain untouched and can accessible through every object;
 
     return 0;
 
