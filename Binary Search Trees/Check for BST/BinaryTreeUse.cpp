68b022ee82113222e7f0a478c1759bb4decf2f60
narendra237 <91244568+narendra237@users.noreply.github.com>
2022-11-18 14:24:26 +0530

    Update BinaryTreeUse.cpp

--- Binary Search Trees/Check for BST/BinaryTreeUse.cpp
+++ Binary Search Trees/Check for BST/BinaryTreeUse.cpp
@@ -269,10 +269,44 @@ bool isBST(BinaryTreeNode<int>* root) {
 	return output;
 }
 
+vector<int> * getRootToNodePath(BinaryTreeNode<int> * root, int data){
+	if(root == NULL)
+		return NULL;
+	
+	if(root->data == data){
+		vector<int> * output = new vector<int>();
+		output->push_back(root->data);
+		return output;
+	}
+
+	vector<int> *leftOutput = getRootToNodePath(root->left, data);
+	if(leftOutput != NULL){
+		leftOutput->push_back(root->data);
+		return leftOutput;
+	}
+
+	vector<int> *rightOutput = getRootToNodePath(root->right, data);
+	if(right != NULL){
+		rightOutput->push_back(root->data);
+		return rightOutput;
+	}
+
+	else
+		return NULL;
+}
+
+
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
 int main() {
 	BinaryTreeNode<int>* root = takeInputLevelWise();
-	cout << isBST3(root) << endl;
+	//cout << isBST3(root) << endl;
+	vector<int> * output = getRootToNodePath(root,8);
+
+    if (output != NULL) {
+        for (int i = 0; i < output->size(); i++) {
+            cout << output->at(i) << " ";
+        }
+    }
 	delete root;
 }
 
